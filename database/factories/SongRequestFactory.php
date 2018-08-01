58187d81428296577a3e7decfa92b9185c544fdd
Casper Boone <mail@casperboone.nl>
2018-08-01 11:02:08 +0200

    Update factory to include a name

--- database/factories/SongRequestFactory.php
+++ database/factories/SongRequestFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\SongRequest::class, function (Faker $faker) {
     return [
+        'name' => $faker->name,
         'track' => $faker->title,
         'artist' => $faker->name,
         'votes' => 0,
