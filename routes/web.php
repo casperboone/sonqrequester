c8970f84eeac358e6d0c2d621274626db71a4994
Casper Boone <mail@casperboone.nl>
2018-08-09 16:42:13 +0200

    Remove public registration

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,16 @@ Route::post('requests', 'SongRequestController@store');
 Route::post('requests/{songRequest}/upvote', 'SongRequestController@upvote');
 Route::post('search', 'SongSearchController@search');
 
-Auth::routes();
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('admin', 'HomeController@index')->name('home');
