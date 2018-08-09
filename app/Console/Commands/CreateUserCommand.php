6a21ea1e600bbe40dd379367737c94a74ab361d7
Casper Boone <mail@casperboone.nl>
2018-08-09 16:30:30 +0200

    Add user create command

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $details = [
+            'name' => $this->ask('What\'s your name?'),
+            'email' => $this->ask('What\'s your email adress?'),
+            'password' => $this->ask('Please enter a password'),
+            'password_confirmation' => $this->ask('Please confirm your password'),
+        ];
+
+        $validator = $this->validator($details);
+        if ($validator->fails()) {
+            $this->error($validator->errors());
+            return;
+        }
+
+        tap($this->create($details), function ($user) {
+            $this->info("User created with id {$user->id}.");
+        });
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
